cce222957a9f1c12d56c4977aef8c5e375ef6646
Cabbage <kiwibirdi@hotmail.com>
2020-01-31 12:52:08 +0100

    wanderstate

---
+++ Caligula/Source/WanderState.cc
@@ -0,0 +1,16 @@
+//WANDERSTATE.CC
+#include "WanderState.h"
+
+void WanderState::Enter()
+{
+	//get position of agent
+	//get a direction
+}
+bool WanderState::Update()
+{
+	//move in that direction
+}
+void WanderState::Exit()
+{
+	//switch state?
+}
